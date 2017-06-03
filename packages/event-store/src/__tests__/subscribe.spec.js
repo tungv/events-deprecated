1e68a0818c3d858e9c06eee944d91282c5876475
Tung Vu <me@tungv.com>
2017-06-03 22:46:10 +0700

    feat: subscribe.js returns factory instead of raw service

--- packages/event-store/src/__tests__/subscribe.spec.js
+++ packages/event-store/src/__tests__/subscribe.spec.js
@@ -1,29 +1,30 @@
-const request = require('request');
-const listen = require('test-listen');
-const micro = require('micro');
-const a = require('awaiting');
-const service = require('../subscribe').default;
+import micro from 'micro';
+
+import { delay } from 'awaiting';
+import listen from 'test-listen';
+import request from 'request';
+
+import { createClient } from '../redis-client';
+import makeSubscribe from '../subscribe';
 
 const kefir = require('kefir');
 
-import { createClient } from '../redis-client';
-
 jest.unmock('micro');
 
-const fromURL = (url) => {
+const fromURL = url => {
   const stream = request(url);
 
   const data$ = kefir.stream(emitter => {
     const emitString = data => {
-      emitter.emit(String(data))
-    }
+      emitter.emit(String(data));
+    };
 
     const errorString = data => {
-      emitter.error(String(data))
-    }
+      emitter.error(String(data));
+    };
 
     const end = () => {
-      emitter.end()
+      emitter.end();
     };
 
     stream.on('data', emitString);
@@ -34,7 +35,7 @@ const fromURL = (url) => {
       stream.removeListener('data', emitString);
       stream.removeListener('error', errorString);
       stream.removeListener('end', end);
-      stream.end()
+      stream.end();
     };
   });
 
@@ -42,28 +43,74 @@ const fromURL = (url) => {
     data$,
     abort: () => stream.abort(),
   };
-}
+};
+
+const takeEvents = (count, stream) =>
+  stream.take(count).scan((prev, next) => prev.concat(next), []).toPromise();
 
 describe('subscribe endpoint', () => {
   it('should subscribe', async () => {
     const pubClient = createClient();
+    const service = makeSubscribe({
+      redis: {},
+      history: { size: 10 },
+    });
     const server = micro(service);
     const url = await listen(server);
-    const {data$, abort} = fromURL(url);
+    const { data$, abort } = fromURL(url);
+
     // 1 -> :ok
     // 2 -> first event
     // 3 -> second event
     // 4 -> abort
-    const promise = data$.take(4).scan((prev, next) => prev.concat(next), []).toPromise();
-    await a.delay(100);
+    const promise = takeEvents(4, data$);
+    await delay(100);
 
     pubClient.publish('events', '1:{"type":"first"}');
     pubClient.publish('events', `2:{"type":"second","payload":123}`);
-    await a.delay(100);
+    await delay(100);
 
     abort();
+    server.close();
     const val = await promise;
-
+    pubClient.quit();
     expect(val).toMatchSnapshot();
+  });
+
+  it('should work with 2 clients', async () => {
+    const pubClient = createClient();
+    const service = makeSubscribe({
+      redis: {},
+      history: { size: 10 },
+    });
+    const server = micro(service);
+    const url = await listen(server);
+
+    const http1 = fromURL(url + '?client=1');
+    const http2 = fromURL(url + '?client=2');
+    await delay(100);
+
+    const promise1 = takeEvents(4, http1.data$);
+    const promise2 = takeEvents(3, http2.data$);
+
+    // publish 2 events
+    // client 1 will wait until the end
+    // client 2 will abort early after the first event
+    pubClient.publish('events', '1:{"type":"first"}');
+
+    await delay(10);
+    http2.abort();
+
+    await delay(10);
+    pubClient.publish('events', `2:{"type":"second"}`);
+
+    await delay(10);
+    http1.abort();
+
+    const val1 = await promise1;
+    const val2 = await promise2;
+
+    expect(val1.length).toBe(2);
+    expect(val2.length).toBe(1);
   });
 });
--- packages/event-store/src/subscribe.js
+++ packages/event-store/src/subscribe.js
@@ -1,19 +1,11 @@
 /* @flow */
 import kefir from 'kefir';
 import mitt from 'mitt';
-import once from 'lodash/fp/once'
+import once from 'lodash/fp/once';
 
 import { parse } from 'querystring';
 
 import { createClient } from './redis-client';
-
-const subClient = createClient();
-subClient.db = 0;
-
-// get notifications for hash keys
-subClient.config('set', 'notify-keyspace-events', 'Kh');
-
-const emitter = mitt();
 
 const tryParse = raw => {
   try {
@@ -22,39 +14,6 @@ const tryParse = raw => {
     return { type: '@@RAW', payload: raw };
   }
 };
-
-const MAX_SIZE = 10;
-const history = [];
-const cache = {};
-const addToCache = (message) => {
-  const newSize = history.unshift(message.id);
-  cache[message.id] = message;
-
-  if (newSize > MAX_SIZE) {
-    const expired = history.splice(MAX_SIZE);
-    expired.forEach(removingId => {
-      delete cache[removingId];
-    });
-  }
-}
-
-
-const message$ = kefir
-  .fromEvents(subClient, 'message', (channel, message) => message)
-  .map(message => {
-    const rawId = message.split(':')[0];
-    const id = Number(rawId);
-    const rawEvent = message.slice(rawId.length + 1);
-
-    return {
-      id,
-      ...tryParse(rawEvent),
-    };
-  })
-  .onValue(addToCache)
-
-
-subClient.subscribe('events');
 
 function flush(response) {
   if (response.flush && response.flush.name !== 'deprecated') {
@@ -68,49 +27,94 @@ data: ${JSON.stringify(event)}
 
 `;
 
-const addClient = (src, opts, match, req, res) => {
-  req.socket.setTimeout(0);
-  req.socket.setNoDelay(true);
-  req.socket.setKeepAlive(true);
+export default ({ redis, history, debug }) => {
+  const subClient = createClient(redis);
 
-  res.writeHead(200, {
-    'Content-Type': 'text/event-stream;charset=UTF-8',
-    'Cache-Control': 'no-cache',
-    Connection: 'keep-alive',
-  });
+  // prepare cache
+  const MAX_SIZE = history.size;
+  const list = [];
+  const cache = {};
 
-  res.write(':ok\n\n');
+  const addToCache = message => {
+    debug && console.log('adding to cache', message.id);
 
-  // cast to integer
-  const retry = opts.retry | 0;
+    const newSize = list.unshift(message.id);
+    cache[message.id] = message;
 
-  if (retry) {
-    res.write('retry: ' + retry + '\n');
-  }
+    debug && console.log('HISTORY', list);
+    debug && console.log('CACHE', cache);
 
-  flush(res);
+    if (newSize > MAX_SIZE) {
+      const expired = list.splice(MAX_SIZE);
+      expired.forEach(removingId => {
+        delete cache[removingId];
+      });
+    }
+  };
 
-  const subscription = src.filter(x => x).map(toOutput).observe(block => {
-    res.write(block);
-  })
+  const message$ = kefir
+    .fromEvents(subClient, 'message', (channel, message) => message)
+    .map(message => {
+      const rawId = message.split(':')[0];
+      const id = Number(rawId);
+      const rawEvent = message.slice(rawId.length + 1);
 
-  const removeClient = once(() => {
-    subscription.unsubscribe();
-    res.end();
-  })
+      return {
+        id,
+        ...tryParse(rawEvent),
+      };
+    })
+    .onValue(addToCache);
 
-  req.on('end', removeClient);
-  req.on('close', removeClient);
-  res.on('finish', removeClient);
-};
+  subClient.subscribe('events');
 
-export default async (req: any, res: any) => {
-  try {
-    const url = req.url;
-    const query = parse(url.split('?')[1]);
+  const addClient = (src, opts, match, req, res) => {
+    req.socket.setTimeout(0);
+    req.socket.setNoDelay(true);
+    req.socket.setKeepAlive(true);
 
-    addClient(message$, {}, query, req, res);
-  } catch (ex) {
-    console.log('ex', ex);
-  }
+    res.writeHead(200, {
+      'Content-Type': 'text/event-stream;charset=UTF-8',
+      'Cache-Control': 'no-cache',
+      Connection: 'keep-alive',
+    });
+
+    res.write(':ok\n\n');
+
+    // cast to integer
+    const retry = opts.retry | 0;
+
+    if (retry) {
+      res.write('retry: ' + retry + '\n');
+    }
+
+    flush(res);
+
+    const subscription = src.filter(x => x).map(toOutput).observe(block => {
+      debug && console.log('send to %s %s', req.url, block);
+      res.write(block);
+    });
+
+    const removeClient = once(() => {
+      debug && console.log('removing', req.url);
+      subscription.unsubscribe();
+      res.end();
+    });
+
+    req.on('end', removeClient);
+    req.on('close', removeClient);
+    res.on('finish', removeClient);
+  };
+
+  return async (req: any, res: any) => {
+    debug && console.log('connected', req.url);
+    try {
+      const url = req.url;
+      const query = parse(url.split('?')[1]);
+
+      addClient(message$, {}, query, req, res);
+    } catch (ex) {
+      console.log('ex', ex);
+    }
+  };
 };
