d1db1eee6563e9de4fa93cd6f1a61b7c9e7782f4
Tung Vu <me@tungv.com>
2017-06-07 22:38:04 +0700

    feat: send SHA1 after the first lua success call

--- packages/event-store/src/runLua.js
+++ packages/event-store/src/runLua.js
@@ -1,5 +1,6 @@
 /* @flow */
 import promisify from 'es6-promisify';
+import crypto from 'crypto';
 
 type EvalParams = {
   keys: ?(string[]),
@@ -8,17 +9,42 @@ type EvalParams = {
 
 type RunLua = (any, string, EvalParams) => Promise<any>;
 
+const sha1 = (text: string): string => {
+  const hash = crypto.createHash('sha1');
+  return hash.update(text).digest('hex');
+};
+
+const map = new Map();
+
 const runLua: RunLua = promisify(
   (redisClient, luaScript: string, { keys, argv }: EvalParams, cb) => {
     const nonEmptyKeys = keys || [];
     const nonEmptyArgv = argv || [];
+
+    const hashOrNull = map.get(luaScript);
+    if (hashOrNull) {
+      return redisClient.evalsha(
+        hashOrNull,
+        nonEmptyKeys.length,
+        ...nonEmptyKeys,
+        ...nonEmptyArgv,
+        cb
+      );
+    }
 
     return redisClient.eval(
       luaScript,
       nonEmptyKeys.length,
       ...nonEmptyKeys,
       ...nonEmptyArgv,
-      cb
+      (err, val) => {
+        if (!err) {
+          const hash = sha1(luaScript);
+          map.set(luaScript, hash);
+        }
+
+        cb(err, val);
+      }
     );
   }
 );
