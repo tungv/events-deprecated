803b97488d83e4567cf2993694f60e2505400bd1
Tung Vu <me@tungv.com>
2017-06-09 15:39:04 +0700

    feat: improve list cmd

--- packages/event-store/src/bin/http-event-list.js
+++ packages/event-store/src/bin/http-event-list.js
@@ -1,11 +1,45 @@
 #!/usr/bin/env node
 /* @flow */
 import pm2 from 'pm2';
-import chalk from 'chalk'
-import pkg from '../../package.json'
+import chalk from 'chalk';
+import pkg from '../../package.json';
 import parseInput from './parseInput';
+import Table from 'cli-table2';
+import prettyBytes from 'pretty-bytes';
+import prettyMs from 'pretty-ms';
+import { pick, forEach, map, toArray, flow, filter, groupBy } from 'lodash/fp';
 
-console.log(chalk.bold(`${pkg.name} start v${pkg.version}`))
+console.log(chalk.bold(`${pkg.name} list v${pkg.version}`));
+
+const print = flow(
+  filter(p => p.name.startsWith('http-event-server-')),
+  groupBy(p => p.name),
+  toArray,
+  forEach(processes => {
+    const name = processes[0].name;
+    const t = new Table({
+      head: ['pid', 'uptime', 'ram', 'cpu'],
+      style: {
+        compact: true,
+        head: ['green'],
+        'padding-left': 2,
+        'padding-right': 2,
+      },
+    });
+
+    processes.forEach(p => {
+      t.push([
+        p.pid,
+        prettyMs(Date.now() - p.pm2_env.created_at),
+        prettyBytes(p.monit.memory),
+        p.monit.cpu,
+      ]);
+    });
+
+    console.log(chalk.bold.green(`\nname: ${name}`));
+    console.log(t.toString());
+  })
+);
 
 pm2.connect(err => {
   if (err) {
@@ -14,15 +48,12 @@ pm2.connect(err => {
   }
 
   pm2.list((err, apps) => {
-    pm2.disconnect();   // Disconnects from PM2
+    pm2.disconnect(); // Disconnects from PM2
 
     if (err) {
       console.error(err);
     } else {
-      // console.log('%s started', input.name)
-      // console.log('running instance(s): %d', apps.length);
-
-      console.log(require('util').inspect(apps, { depth: 2 }));
+      print(apps);
     }
   });
 });
