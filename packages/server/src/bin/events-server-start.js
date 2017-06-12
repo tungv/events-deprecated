4b1b1ddc2e3e65258a7990d7900cc2a127f440ac
Tung Vu <me@tungv.com>
2017-06-12 16:31:10 +0700

    fix: resolve pathname to server.js

--- packages/server/src/bin/events-server-start.js
+++ packages/server/src/bin/events-server-start.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 /* @flow */
 import pm2 from 'pm2';
-import chalk from 'chalk'
-import pkg from '../../package.json'
+import chalk from 'chalk';
+import path from 'path';
+import pkg from '../../package.json';
 import parseInput from './parseInput';
 
-console.log(chalk.bold(`${pkg.name} start v${pkg.version}`))
+console.log(chalk.bold(`${pkg.name} start v${pkg.version}`));
 
 const input = parseInput();
 
@@ -15,21 +16,24 @@ pm2.connect(err => {
     process.exit(2);
   }
 
-  pm2.start({
-    script: './build/bin/server.js',
-    name: `events-server-${input.name}`,
-    args: process.argv.slice(2),
-    exec_mode: 'cluster',
-    instances: input.workers || 0,
-    max_memory_restart: '100M'
-  }, (err, apps) => {
-    pm2.disconnect();   // Disconnects from PM2
+  pm2.start(
+    {
+      script: path.resolve(__dirname, './server.js'),
+      name: `events-server-${input.name}`,
+      args: process.argv.slice(2),
+      exec_mode: 'cluster',
+      instances: input.workers || 0,
+      max_memory_restart: '100M',
+    },
+    (err, apps) => {
+      pm2.disconnect(); // Disconnects from PM2
 
-    if (err) {
-      console.error(err);
-    } else {
-      console.log('%s started', input.name)
-      console.log('running instance(s): %d', apps.length);
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('%s started', input.name);
+        console.log('running instance(s): %d', apps.length);
+      }
     }
-  });
+  );
 });
