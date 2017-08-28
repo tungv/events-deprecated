f2545e7761ac796d20d696c5df58e0291bf69381
Tung Vu <me@tungv.com>
2017-08-28 23:05:19 +0700

    feat: add aliases

--- packages/cli/src/es.js
+++ packages/cli/src/es.js
@@ -7,7 +7,17 @@ import mri from 'mri';
 import { h, render } from 'ink';
 import App from './components/App';
 
-const args = mri(process.argv.slice(2));
+const args = mri(process.argv.slice(2), {
+  alias: {
+    R: 'filter',
+    Y: 'yes',
+    w: 'workers',
+    p: 'port',
+    r: 'redis',
+    T: 'burstTime',
+    C: 'burstCount',
+  },
+});
 
 const command = args._.shift();
 
