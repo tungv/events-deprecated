5ac907c126576ca6b22e030eab00208ecb79f4e3
Tung Vu <me@tungv.com>
2017-08-30 17:35:18 +0700

    chore: add version to factory

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -5,32 +5,39 @@ import makeCors from 'micro-cors';
 import micro from 'micro';
 
 import { createClient } from './redis-client';
+import { name, version } from '../package.json';
 
 const cors = makeCors();
 
 import type { Config } from '../types/Config.type';
 import commit from './commit';
 import subscribe from './subscribe';
-import query from './query'
+import query from './query';
 
 export default (config: Config) => {
+  console.log('%s version %s is starting...', name, version);
   const statusClient = createClient(config.redis, { debug: true });
   const committer = commit(config);
-  const {service: subscriber, unsubscribe} = subscribe(config);
+  const { service: subscriber, unsubscribe } = subscribe(config);
   const querier = query(config);
 
-  const service = handleErrors(cors(router(
-    get('/subscribe', subscriber),
-    post('/commit', committer),
-    get('/query', querier),
-    () => {
-      throw createError(404)
-    }
-  )), config.debug);
+  const service = handleErrors(
+    cors(
+      router(
+        get('/subscribe', subscriber),
+        post('/commit', committer),
+        get('/query', querier),
+        () => {
+          throw createError(404);
+        }
+      )
+    ),
+    config.debug
+  );
 
   const server = micro(service);
 
   server.on('close', unsubscribe);
 
   return server;
-}
+};
