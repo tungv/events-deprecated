7c814c303bb582d80b4acc5326836506ac37d6ae
Tung Vu <me@tungv.com>
2017-09-04 14:08:49 +0700

    feat: add log

--- packages/subscriber-cli/src/nonTTY.js
+++ packages/subscriber-cli/src/nonTTY.js
@@ -1,13 +1,34 @@
+/* @flow */
+import prettyMs from 'pretty-ms';
 import subscribe from '@events/subscriber';
 
-export default (async function({ url, lastEventId, burstTime, burstCount }) {
+type Props = {
+  url: string,
+  lastEventId: number,
+  burstCount: number,
+  burstTime: number,
+};
+
+const write = (...args) => console.error('SUBSCRIBER:', ...args);
+
+export default (async function({
+  url,
+  lastEventId,
+  burstTime,
+  burstCount,
+}: Props) {
+  write(`connecting to ${url}`);
+  const connectingTS = Date.now();
   const { raw$, events$, abort } = subscribe(url, {
     'Last-Event-ID': lastEventId,
     'burst-count': burstCount,
     'burst-time': burstTime,
   });
 
-  process.on('SIGINT', abort);
+  process.on('SIGINT', () => {
+    write('aborting...');
+    abort();
+  });
 
   const firstMessage = await raw$.take(1).toPromise();
   if (firstMessage.slice(0, 3) !== ':ok') {
@@ -15,5 +36,16 @@ export default (async function({ url, lastEventId, burstTime, burstCount }) {
     return;
   }
 
-  events$.onValue(json => process.stdout.write(`${JSON.stringify(json)}\n`));
+  raw$.onError(error => {
+    write(`Error while subscribing to ${url}`);
+    write(error);
+  });
+
+  raw$.onEnd(() => {
+    write('stream ended');
+    process.exit(1);
+  });
+
+  write(`connected after ${prettyMs(Date.now() - connectingTS)}!`);
+  events$.onValue(json => console.log(`${JSON.stringify(json)}`));
 });
