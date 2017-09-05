ce4b0ab6a56bc941653c725ce176d034927d83ad
Tung Vu <me@tungv.com>
2017-09-05 15:52:23 +0700

    feat: implement generic snapshot-cli

---
+++ packages/snapshot/src/index.js
@@ -0,0 +1,15 @@
+const mri = require('mri');
+const noop = require('lodash/fp/noop');
+
+const input = mri(process.argv.slice(2), { alias: { x: 'debug' } });
+
+const cmd = input._.shift();
+
+const write = input.debug ? str => console.error('[SNAPSHOT] ', str) : noop;
+
+try {
+  const program = require(`./${cmd}`);
+  program(input, write).then(process.exit.bind(0), process.exit.bind(1));
+} catch (ex) {
+  write(`unknown command ${cmd}`);
+}
---
+++ packages/snapshot/src/version.js
@@ -0,0 +1,42 @@
+const { URL } = require('url');
+
+module.exports = version;
+
+const packageByProtocol = {
+  'mongodb:': '@events/snapshot-mongo',
+};
+
+async function version(input, write) {
+  const datasource = input._[0];
+
+  if (!datasource) {
+    write(`please specific a datasource\nsnapshot version [datasource]`);
+    throw new Error('no datasource');
+  }
+
+  const url = new URL(datasource);
+
+  if (!url.protocol) {
+    write(`please specific a protocol in datasource`);
+    throw new Error('no protocol');
+  }
+
+  const moduleName = packageByProtocol[url.protocol];
+  if (!moduleName) {
+    write(`${url.protocol} is unknown`);
+    throw new Error('unknown protocol');
+  }
+
+  let m;
+
+  try {
+    m = require(moduleName);
+  } catch (ex) {
+    write(`cannot resolve package ${moduleName} from ${process.cwd()}. Make sure you install it by:
+    npm install ${moduleName}
+    OR
+    yarn add ${moduleName}
+`);
+    throw new Error('module not found');
+  }
+}
