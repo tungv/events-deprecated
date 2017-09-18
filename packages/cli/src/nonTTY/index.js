57024bc1f97813fc9fcafae1933170411ca47d37
Tung Vu <me@tungv.com>
2017-09-18 15:25:42 +0700

    feat: non-tty start/stop

--- packages/cli/src/nonTTY/index.js
+++ packages/cli/src/nonTTY/index.js
@@ -11,40 +11,53 @@ export default async (command, args) => {
     ...args,
   };
 
-  if (command !== 'start' || args.noDaemon !== true) {
-    console.error('command %s --noDaemon is not supported in non-TTY', command);
-    process.exit(1);
-    return;
+  const disconnect = await connect();
+  console.log(`%s %s version %s`, name, command, version);
+  if (command === 'start') {
+    const app = await startApp(
+      argsWithDefault.name,
+      argsWithDefault,
+      Number(argsWithDefault.workers),
+      !argsWithDefault.noDaemon
+    );
+
+    console.log(
+      '%s started %d instance(s)',
+      argsWithDefault.name,
+      app.instances.length
+    );
+
+    if (!argsWithDefault.noDaemon) {
+      disconnect();
+      process.exit(0);
+    }
+
+    let stopped = false;
+    process.on('SIGINT', async () => {
+      await stopApp(argsWithDefault.name);
+      console.log('%s stopped', argsWithDefault.name);
+      stopped = true;
+      disconnect();
+      process.exit(0);
+    });
+
+    process.on('exit', async () => {
+      if (stopped) return;
+      await stopApp(argsWithDefault.name);
+      console.log('%s stopped', argsWithDefault.name);
+      disconnect();
+    });
   }
 
-  console.log(`%s %s version %s`, name, command, version);
-  const disconnect = await connect();
-  const app = await startApp(
-    argsWithDefault.name,
-    argsWithDefault,
-    Number(argsWithDefault.workers),
-    true
-  );
-
-  console.log(
-    '%s started %d instance(s)',
-    argsWithDefault.name,
-    app.instances.length
-  );
-
-  let stopped = false;
-  process.on('SIGINT', async () => {
-    await stopApp(argsWithDefault.name);
-    console.log('%s stopped', argsWithDefault.name);
-    stopped = true;
-    disconnect();
-    process.exit(0);
-  });
-
-  process.on('exit', async () => {
-    if (stopped) return;
-    await stopApp(argsWithDefault.name);
-    console.log('%s stopped', argsWithDefault.name);
-    disconnect();
-  });
+  if (command === 'stop') {
+    try {
+      await stopApp(argsWithDefault._[0]);
+      disconnect();
+      process.exit(0);
+    } catch (ex) {
+      console.log(ex.message);
+      disconnect();
+      process.exit(1);
+    }
+  }
 };
