6c25983392fcb540374548e9c6e25a359992cf4f
Tung Vu <me@tungv.com>
2017-12-19 18:19:07 +0700

    feat: cache mongo client by url

--- packages/snapshot-mongo/src/connect.js
+++ packages/snapshot-mongo/src/connect.js
@@ -1,8 +1,14 @@
 import MongoHeartbeat from 'mongo-heartbeat';
 import { MongoClient } from 'mongodb';
 
+const cache = {};
+
 export default async url => {
   try {
+    if (cache[url]) {
+      return cache[url];
+    }
+
     const db = await MongoClient.connect(url);
     const hb = MongoHeartbeat(db, {
       interval: 5000,
@@ -16,6 +22,8 @@ export default async url => {
         process.exit(1);
       });
     });
+
+    cache[url] = db;
 
     return db;
   } catch (ex) {
