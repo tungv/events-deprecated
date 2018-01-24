392f46db7d7717d49144d23a5aefab23b3ea8005
Tung Vu <me@tungv.com>
2018-01-24 10:25:25 +0700

    test: add test for multiple events

--- packages/snapshot-mongo/src/__tests__/persist.spec.js
+++ packages/snapshot-mongo/src/__tests__/persist.spec.js
@@ -7,7 +7,7 @@ const { MONGO_TEST } = process.env;
 const bulkWriteModule = require('../bulkWrite');
 const bulkWriteSpy = jest.spyOn(bulkWriteModule, 'default');
 
-const fromArray = array => kefir.sequentially(1, array);
+const fromArray = array => kefir.sequentially(0, array);
 
 const toArray = stream$ =>
   stream$.scan((array, item) => array.concat(item), []).toPromise();
@@ -18,7 +18,7 @@ const dropDB = async url => {
 };
 
 const getPersistenceResults = async ({
-  config = { _: [MONGO_TEST] },
+  config = { _: [MONGO_TEST], buffer: { enabled: false } },
   projections = [],
   collections = [],
 }) => {
@@ -31,7 +31,11 @@ const getPersistenceResults = async ({
 };
 
 describe('persist', () => {
-  test("persist a single event's projection", async () => {
+  beforeEach(() => {
+    bulkWriteSpy.mockClear();
+  });
+
+  it("should persist a single event's projection", async () => {
     // reset
     await dropDB(MONGO_TEST);
 
@@ -94,5 +98,74 @@ describe('persist', () => {
     ]);
 
     expect(out[0].changes).toBe(2);
+  });
+
+  it('should persist 2 events', async () => {
+    await dropDB(MONGO_TEST);
+
+    const projections = [
+      {
+        event: { id: 1 },
+        projections: {
+          col1: [{ __pv: '1.0.0', op: { insert: [{ x: 1 }] } }],
+          col2: [{ __pv: '1.0.0', op: { insert: [{ y: 2 }] } }],
+        },
+      },
+      {
+        event: { id: 2 },
+        projections: {
+          col1: [
+            {
+              __pv: '1.0.0',
+              op: { update: { where: { x: 1 }, changes: { $set: { x: 2 } } } },
+            },
+          ],
+        },
+      },
+    ];
+
+    const collections = [
+      { name: 'col1', version: '1.0.0' },
+      { name: 'col2', version: '1.0.0' },
+    ];
+
+    const out = await getPersistenceResults({ projections, collections });
+
+    const spyCalls = bulkWriteSpy.mock.calls;
+
+    expect(spyCalls.map(args => args[0].collectionName)).toEqual([
+      '__snapshots_v1.0.0',
+      'col1_v1.0.0',
+      'col2_v1.0.0',
+      'col1_v1.0.0',
+    ]);
+
+    expect(spyCalls[1][1]).toEqual([
+      {
+        updateOne: {
+          filter: { __v: { $gte: 1 } },
+          update: { $setOnInsert: { __v: 1, x: 1 } },
+          upsert: true,
+        },
+      },
+    ]);
+    expect(spyCalls[2][1]).toEqual([
+      {
+        updateOne: {
+          filter: { __v: { $gte: 1 } },
+          update: { $setOnInsert: { __v: 1, y: 2 } },
+          upsert: true,
+        },
+      },
+    ]);
+    expect(spyCalls[3][1]).toEqual([
+      {
+        updateMany: {
+          filter: { __v: { $lt: 2 }, x: 1 },
+          update: { $set: { __v: 2, x: 2 } },
+          upsert: false,
+        },
+      },
+    ]);
   });
 });
