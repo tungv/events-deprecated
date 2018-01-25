29a34418d0345c5d4b74474e1977eec2e228a18a
Tung Vu <me@tungv.com>
2018-01-25 16:15:23 +0700

    feat: logger

---
+++ packages/client2/src/logger/index.js
@@ -0,0 +1,37 @@
+const LOG_LEVEL = {
+  SILLY: 1,
+  DEBUG: 2,
+  INFO: 3,
+  WARN: 4,
+  ERROR: 5,
+  FATAL: 6,
+  DATA: 10,
+};
+
+let CURRENT_LOG_LEVEL;
+let USE_JSON = true;
+
+const JSONReporter = (level, data) => {
+  data._l = level;
+  data._t = Date.now();
+
+  return JSON.stringify(data);
+};
+
+export const setLogLevel = nextLevel => (CURRENT_LOG_LEVEL = nextLevel);
+
+export const shouldLog = level => {
+  const minLogLevel = LOG_LEVEL[CURRENT_LOG_LEVEL];
+
+  return LOG_LEVEL[level] >= minLogLevel;
+};
+
+export const write = (level, content) => {
+  if (!shouldLog(level)) {
+    return;
+  }
+
+  const reporter = JSONReporter;
+
+  console.log(reporter(level, content));
+};
--- packages/client2/src/subscribe.js
+++ packages/client2/src/subscribe.js
@@ -1,17 +1,32 @@
 import parseConfig from './utils/parseConfig';
 import path from 'path';
+import { setLogLevel, write } from './logger';
+
 const { params } = process.env;
 
 const { json, verbose, configPath } = JSON.parse(params);
 
+setLogLevel(verbose);
+
 prepare().then(loop, handleErrors);
 
 async function prepare() {
-  console.log('prepare');
+  write('INFO', {
+    type: 'load-config-begin',
+    payload: {
+      path: configPath,
+    },
+  });
+
   const configDir = path.resolve(configPath, '..');
   const config = await parseConfig(require(configPath), configDir);
 
-  console.log(require('util').inspect(config, { depth: null }));
+  write('DEBUG', {
+    type: 'load-config-end',
+    payload: {
+      config,
+    },
+  });
 }
 
 async function loop() {}
