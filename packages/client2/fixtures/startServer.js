f9d6b723996f665c8cc96cc39b89826d8f07ed5a
Tung Vu <me@tungv.com>
2018-01-26 10:45:04 +0700

    test: do not flushdb

--- packages/client2/fixtures/startServer.js
+++ packages/client2/fixtures/startServer.js
@@ -1,17 +1,18 @@
 import factory from '@events/server';
 import nodeRedis from 'redis';
-import flushdb from 'redis-functional/flushdb';
+import del from 'redis-functional/del';
 import enableDestroy from 'server-destroy';
 
 export default function({ namespc, redis, port, clean = false }) {
   let p = Promise.resolve(true);
 
   if (clean) {
-    p = p.then(() => {
+    p = p.then(async () => {
       const client = nodeRedis.createClient(redis);
-      return flushdb(client).then(() => {
-        client.quit();
-      });
+      await del(client, `{${namespc}}::id`);
+      await del(client, `{${namespc}}::events`);
+
+      client.quit();
     });
   }
 
