b2d9cc41f1cf8b46320b4722abbef785636d01f2
Tung Vu <me@tungv.com>
2018-01-29 11:40:37 +0700

    fix: monitor and better --verbose message

--- packages/client2/src/cli.js
+++ packages/client2/src/cli.js
@@ -13,8 +13,8 @@ program
   .option('--json', 'output logs in JSON format', false)
   .option(
     '--verbose, -x',
-    'level of log, from 0 (no log) to 10 (log everything)',
-    'INFO',
+    'level of log can be either SILLY, DEBUG, INFO, WARN, ERROR, or FATAL',
+    'INFO'
   );
 
 program
@@ -22,7 +22,7 @@ program
     default: true,
   })
   .describe(
-    'start to subscribe to an heq server and persist data to a datastore',
+    'start to subscribe to an heq server and persist data to a datastore'
   )
   .option('--config, -c', 'path to config file', 'events.config.js')
   .example('subscribe --config path/to/config.js')
--- packages/client2/src/subscribeCommand.js
+++ packages/client2/src/subscribeCommand.js
@@ -1,4 +1,5 @@
 import execa from 'execa';
+import { createServer } from 'http';
 import prettyMs from 'pretty-ms';
 
 import path from 'path';
@@ -7,14 +8,39 @@ export default async ({ json, verbose, config }) => {
   const startTime = Date.now();
   // start a subscribe worker
   const configPath = path.resolve(process.cwd(), config);
-
-  const params = JSON.stringify({ json, verbose, configPath });
+  const params = JSON.stringify({
+    json,
+    verbose: verbose || 'INFO',
+    configPath,
+  });
   const executable = path.resolve(__dirname, './subscribe.js');
 
   process.on('SIGINT', () => {
     // maximum 1s for worker to cleanup
     setTimeout(process.exit, 1000, 0);
   });
+
+  const simpleConfig = require(configPath);
+  if (simpleConfig.monitor && simpleConfig.monitor.port) {
+    const server = createServer((req, res) => {
+      res.statusCode = 200;
+      res.end('ok');
+    });
+    server.listen(simpleConfig.monitor.port, err => {
+      if (err) {
+        console.error(
+          'cannot start http server on port %s. Exitting...',
+          simpleConfig.monitor.port
+        );
+        process.exit(1);
+        return;
+      }
+      console.log(
+        'monitoring server is listening on port',
+        simpleConfig.monitor.port
+      );
+    });
+  }
 
   const worker = execa('node', ['-r', 'babel-register', executable], {
     env: { params },
